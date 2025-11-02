493081c33afbd1cc5ea90e8a33b6e97fa9f40260
the y team <yer.3211@gmail.com>
2025-11-02 13:31:40 +0200

    fix robotsubsytem problem with 2 types of elevator

--- src/main/java/frc/robot/RobotSubsystems.java
+++ src/main/java/frc/robot/RobotSubsystems.java
@@ -1,10 +1,14 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.RobotBase;
 import frc.robot.subsystems.Aempitch.armPitch;
 import frc.robot.subsystems.IntakeIndexer.IntakeIndexer;
 import frc.robot.subsystems.IntakePitch.IntakePitch;
 import frc.robot.subsystems.armGruper.ArmGriper;
 import frc.robot.subsystems.climb.ClimbSubsystem;
+import frc.robot.subsystems.elevatorsim.elevator2;
+import frc.robot.subsystems.elevatorsim.elevatorIOreal;
+import frc.robot.subsystems.elevatorsim.elevatorIOsim;
 import frc.robot.subsystems.elvetor.elevator;
 import frc.robot.subsystems.intakeGriper.IntakeGriper;
 import frc.robot.subsystems.swerve.SwerveSubsystem;
@@ -12,6 +16,7 @@ import frc.robot.subsystems.swerve.SwerveSubsystem;
 public class RobotSubsystems {
   // subsystems
   public final elevator elevator;
+  // public final elevator2 elevator2;
   public final ArmGriper ArmGriper;
   public final armPitch armpitch;
   public final IntakeGriper IntakeGriper;
@@ -21,24 +26,18 @@ public class RobotSubsystems {
   public final ClimbSubsystem ClimbSubsystem;
 
   public RobotSubsystems() {
-    if (RobotBase.isReal()) {
-      ArmGriper = new ArmGriper();
-      armpitch = new armPitch();
-      swerve = new SwerveSubsystem();
-      IntakeGriper = new IntakeGriper();
-      intakepitch = new IntakePitch();
-      intakeindexer = new IntakeIndexer();
-      elevator = new elevator2(new elevatorIOreal());
-      ClimbSubsystem = new ClimbSubsystem();
-    } else {
-      ArmGriper = new ArmGriper();
-      armpitch = new armPitch();
-      swerve = new SwerveSubsystem();
-      IntakeGriper = new IntakeGriper();
-      intakepitch = new IntakePitch();
-      intakeindexer = new IntakeIndexer();
-      elevator = new elevator2(new elevatorIOsim());
-      ClimbSubsystem = new ClimbSubsystem();
-    }
+    // if (RobotBase.isReal()) {
+    //   elevator2 = new elevator2(new elevatorIOreal());
+    // } else {
+    //   elevator2 = new elevator2(new elevatorIOsim());
+    // }
+    elevator = new elevator();
+    ClimbSubsystem = new ClimbSubsystem();
+    ArmGriper = new ArmGriper();
+    armpitch = new armPitch();
+    swerve = new SwerveSubsystem();
+    IntakeGriper = new IntakeGriper();
+    intakepitch = new IntakePitch();
+    intakeindexer = new IntakeIndexer();
   }
 }
