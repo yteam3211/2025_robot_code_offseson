315d7a36e883a8e026f31b1f9f184c2397783c1c
avrahamavraham <148977300+avrahamavraham@users.noreply.github.com>
2025-11-06 09:26:55 +0200

    Update driverButtom.java

--- src/main/java/frc/robot/Buttons/driverButtom.java
+++ src/main/java/frc/robot/Buttons/driverButtom.java
@@ -1,27 +1,28 @@
 package frc.robot.Buttons;
 
+import frc.robot.Buttons.SubButton;
 import frc.robot.Controller;
 import frc.robot.commands.ScoreCommands;
 
 public class driverButtom {
 
   public static void loadButtons(Controller controller, ScoreCommands scoreCommands) {
-    // controller
-    //     .swervecontroller
-    //     .povUp()
-    //     .onTrue(scoreCommands.intakeStraitToArm().until(SubButton.test));
+    controller
+         .swervecontroller
+         .povUp()
+         .onTrue(scoreCommands.intakeStraitToArm().until(SubButton.test));
     controller
         .swervecontroller
         .povDown()
         .onTrue(scoreCommands.intakeStayOnIntake().until(SubButton.test));
-    controller.swervecontroller.triangle().onTrue(scoreCommands.ScoreL1());
-    // controller
-    //     .swervecontroller
-    //     .povRight()
-    //     .onTrue(scoreCommands.getClosestLeftRightPose(sideScore.right).until(SubButton.test));
-    // controller
-    //     .swervecontroller
-    //     .povLeft()
+    
+     controller
+        .swervecontroller
+       .povRight()
+       .onTrue(scoreCommands.getClosestLeftRightPose(sideScore.right).until(SubButton.test));
+    controller
+        .swervecontroller
+        .povLeft()
     //     .onTrue(scoreCommands.getClosestLeftRightPose(sideScore.left).until(SubButton.test));
     controller.swervecontroller.cross().whileTrue(scoreCommands.climbslow().until(SubButton.test));
     controller.swervecontroller.circle().whileTrue(scoreCommands.climbfast().until(SubButton.test));
