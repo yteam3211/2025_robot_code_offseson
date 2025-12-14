cd32a8edab8a51803234e618212ae49d262eeaf2
avrahamavraham <avrahamgen@gmail.com>
2025-12-14 20:20:46 +0200

    add maple sim to project

--- src/main/java/frc/robot/RobotSubsystems.java
+++ src/main/java/frc/robot/RobotSubsystems.java
@@ -1,7 +1,14 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Kilogram;
+import static edu.wpi.first.units.Units.Meter;
+import static edu.wpi.first.units.Units.Meters;
+
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.RobotBase;
 import frc.robot.subsystems.Aempitch.armPitch;
 import frc.robot.subsystems.IntakeIndexer.IntakeIndexer;
@@ -13,7 +20,7 @@ import frc.robot.subsystems.climb.ClimbSubsystem;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.drive.GyroIO;
 import frc.robot.subsystems.drive.GyroIOCanAndGyro;
-import frc.robot.subsystems.drive.ModuleIOSim;
+import frc.robot.subsystems.drive.ModuleIOMaple;
 import frc.robot.subsystems.drive.ModuleIOTalonFX;
 import frc.robot.subsystems.drive.TunerConstants;
 import frc.robot.subsystems.elevatorsim.elevator2;
@@ -23,6 +30,10 @@ import frc.robot.subsystems.intakeGriper.IntakeGriper;
 import frc.robot.subsystems.vision.Vision;
 import frc.robot.subsystems.vision.VisionIOLimelight;
 import frc.robot.subsystems.vision.VisionIOPhotonVisionSim;
+import org.ironmaple.simulation.SimulatedArena;
+import org.ironmaple.simulation.drivesims.COTS;
+import org.ironmaple.simulation.drivesims.SwerveDriveSimulation;
+import org.ironmaple.simulation.drivesims.configs.DriveTrainSimulationConfig;
 
 public class RobotSubsystems {
   // subsystems
@@ -38,6 +49,7 @@ public class RobotSubsystems {
   public final Drive drive;
   public final ClimbSubsystem ClimbSubsystem;
   public final Vision vision;
+  private DriveTrainSimulationConfig driveTrainSimulationConfig = null;
 
   public RobotSubsystems() {
     if (RobotBase.isReal()) {
@@ -56,13 +68,29 @@ public class RobotSubsystems {
       elevator2 = new elevator2(new elevatorIOreal());
       IntakePitchSim = new IntakePitchSim(new IntakePitchIOreal());
     } else {
+      driveTrainSimulationConfig =
+          DriveTrainSimulationConfig.Default()
+              .withGyro(COTS.ofPigeon2())
+              .withRobotMass(Kilogram.of(Drive.ROBOT_MASS_KG))
+              .withBumperSize(
+                  Meter.of(Math.abs(TunerConstants.FrontRight.LocationX) + 0.04),
+                  Meter.of(Math.abs(TunerConstants.FrontRight.LocationY) + 0.04))
+              .withSwerveModule(
+                  COTS.ofMark4i(
+                      DCMotor.getFalcon500(1), DCMotor.getKrakenX60(1), Drive.WHEEL_COF, 2))
+              .withTrackLengthTrackWidth(
+                  Meters.of(Math.abs(TunerConstants.FrontRight.LocationX)),
+                  Meter.of(Math.abs(TunerConstants.FrontRight.LocationY)));
+      SwerveDriveSimulation driveSimulation =
+          new SwerveDriveSimulation(driveTrainSimulationConfig, new Pose2d(2, 2, new Rotation2d()));
+      SimulatedArena.getInstance().addDriveTrainSimulation(driveSimulation);
       drive =
           new Drive(
               new GyroIO() {},
-              new ModuleIOSim(TunerConstants.FrontLeft),
-              new ModuleIOSim(TunerConstants.FrontRight),
-              new ModuleIOSim(TunerConstants.BackLeft),
-              new ModuleIOSim(TunerConstants.BackRight));
+              new ModuleIOMaple(driveSimulation.getModules()[0]),
+              new ModuleIOMaple(driveSimulation.getModules()[1]),
+              new ModuleIOMaple(driveSimulation.getModules()[2]),
+              new ModuleIOMaple(driveSimulation.getModules()[3]));
       vision =
           new Vision(
               drive::addVisionMeasurement,
--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -68,9 +68,9 @@ public class Drive extends SubsystemBase {
               Math.hypot(TunerConstants.BackRight.LocationX, TunerConstants.BackRight.LocationY)));
 
   // PathPlanner config constants
-  private static final double ROBOT_MASS_KG = 74.088;
+  public static final double ROBOT_MASS_KG = 74.088;
   private static final double ROBOT_MOI = 6.883;
-  private static final double WHEEL_COF = 1.2;
+  public static final double WHEEL_COF = 1.2;
   private static final RobotConfig PP_CONFIG =
       new RobotConfig(
           ROBOT_MASS_KG,
